84d73f118d68ffbcf1dd0f6b0d3c1affe16b5f65
deandt100 <dean100@live.co.za>
2016-01-26 11:03:03 +0200

    hi

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404
+{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fmodern\fcharset0 Courier-Bold;}
+{\colortbl;\red255\green255\blue255;\red43\green139\blue39;\red26\green26\blue26;\red133\green35\blue39;
+\red18\green139\blue2;\red0\green0\blue83;\red251\green0\blue7;\red0\green0\blue135;\red162\green165\blue6;
+\red0\green0\blue213;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+#include <stdio.h>\cf3 \
+\'a0\
+\pard\pardeftab720\partightenfactor0
+\cf4 int\cf3  main\cf5 ()\cf3 \
+\cf5 \{\cf3 \
+  \cf6 printf\cf5 (\cf7 "Hello world
+\f1\b \cf8 \\n
+\f0\b0 \cf7 "\cf5 )\cf2 ;\cf3 \
+  \cf9 return\cf3  \cf10 0\cf2 ;\cf3 \
+\cf5 \}}
